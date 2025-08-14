2273c14540160eddb9dc134b38111d1efe059ac6
Sandeep Sarkar <152861071+sandy4242@users.noreply.github.com>
2025-08-15 01:49:40 +0530

    learned variables and initial body of c++ code

---
+++ variables/numbers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int b = 10;
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+    return 0;
+}
---
+++ variables/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 10;
+
+    cout<<"addition of a and b is: " << (a + b) << endl;
+    cout<<"subtraction of a and b is: "<< (b-a) << endl;
+    cout<<"multiplication of a and b is: "<< (a * b) << endl;
+    cout<<"division of b by a is: "<< (b / a) << endl;
+    return 0;
+}
